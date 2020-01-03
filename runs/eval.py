e1ce2951bbb30a59c8cbf6019a35bf59430e5580
Yoshihiko Ueno <windows7home@hotmail.co.jp>
2020-01-03 18:32:26 +0900

    fixed 'visible_devices' ignored issue.

--- runs/eval.py
+++ runs/eval.py
@@ -71,6 +71,7 @@ def main(_):
             if FLAGS.visible_device_index != -1:
                 num_gpu = 1
                 index_list = [FLAGS.visible_device_index]
+                os.environ['CUDA_VISIBLE_DEVICES'] = str(FLAGS.visible_device_index)
             else: num_gpu = len(util_ops.get_devices())
     else:
         os.environ['CUDA_VISIBLE_DEVICES'] = ''
